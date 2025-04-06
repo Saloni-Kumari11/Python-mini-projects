7bf6a6c851006d71c830d0c3564d255434a69e70
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-04-06 12:17:16 +0530

    banking

---
+++ Function/script1.py
@@ -0,0 +1,8 @@
+def say_hello():
+    print("Hello, World!")
+
+print("This runs no matter what!")
+
+if __name__ == "__main__":
+    print("This runs only when executed directly!")
+    say_hello()
